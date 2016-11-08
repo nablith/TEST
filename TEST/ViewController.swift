cfed64f5d63d94f7f26c91928ad26bc49358ce46
Richard Ziegler <nablith@gmail.com>
2016-11-08 05:17:03 -0600

    Line 12 on AppDelegate is bugging out for no apparent reason.
    “class AppDelegate: UIResponder, UIApplicationDelegate {“

--- TEST/ViewController.swift
+++ TEST/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
 
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        print(text1.text!)
+        print(text2.text!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
